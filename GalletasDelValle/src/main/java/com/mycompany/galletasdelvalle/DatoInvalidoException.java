1e7816f43dc9c579ebdcd2d3f64692d2d4ea18b3
DaniJim01 <danieljimenez160406@gmail.com>
2025-10-26 14:56:52 -0600

    Update DatoInvalidoException.java

--- GalletasDelValle/src/main/java/com/mycompany/galletasdelvalle/DatoInvalidoException.java
+++ GalletasDelValle/src/main/java/com/mycompany/galletasdelvalle/DatoInvalidoException.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.galletasdelvalle;
 
 public class DatoInvalidoException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L; //Establece una version unica de serializacion para evitar problemas de compatibilidad
 
-    public DatoInvalidoException(String mensaje) {
-        super(mensaje);
+    public DatoInvalidoException(String mensaje) { //Metodo caundo hay un dato invalido
+        super(mensaje); //Envia el mensaje en caso de error de forma prioritaria (Super)
     }
+
 }
